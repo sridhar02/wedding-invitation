8d1eab0a72da1b3c97fce85b67a07d2a5d1be49b
sridhar02 <kattasridhar02@gmail.com>
2021-06-27 10:57:07 +0530

    changes for meta

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).on('click', function () {
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 1, 2021 22:42:00").getTime();
+var countDownDate = new Date("Jul 1, 2021 21:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
